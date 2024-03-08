ba4bb398c267840d39083cce7cd72a3843061724
Santiago Largo <santiagolargo0118@gmail.com>
2024-03-07 21:32:18 -0500

    seguimos en proceso

--- Prueba.py
+++ Prueba.py
@@ -2,6 +2,9 @@
 
 import random
 
+victorias=0
+derrotas=0
+
 while True:
     print('Bienvenidos a mi casino')
     jugador = random.randint(2, 26)
@@ -9,28 +12,34 @@ while True:
     
 
     while True:
-        print(jugador,dealer)
+        print('jugador:',jugador, 'dealer:',dealer)
         quiere_mas_cartas = input ('quiere mas cartas? S/N')
         if quiere_mas_cartas =='S':
             nueva_carta=random.randint(1,13)
-            jugador = jugador + nueva_carta
-            print(jugador,dealer)
+            jugador += nueva_carta
+            print("nueva carta para el jugador:", nueva_carta)
         else:
             if jugador == 21 or (jugador > dealer and jugador > 21):
                 print ("Ganaste!")
+                victorias += 1
                 break
             elif jugador == dealer:
                 print('Perdiste')
+                derrotas +=1
                 break
             else:
                 print('Perdiste')
+                derrotas +=1
                 break
     preguntar_si_salir= input('desea continuar el juego? S/N')
     if preguntar_si_salir == 'S':
             print('gracias por jugar')
+            print('total de victorias:', victorias)
+            print('Total de derrotas', derrotas)
             break
     else: 
             print('\n'*20)
+    #juego.append
                 
          
 
